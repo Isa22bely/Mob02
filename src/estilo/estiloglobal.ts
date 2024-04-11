6b182536aaff473d6c84c185f89d1965e3a29283
Isa22bely <isabelyvga22@gmail.com>
2024-04-11 08:15:43 -0300

    Naegação com Stack

---
+++ src/estilo/estiloglobal.ts
@@ -0,0 +1,9 @@
+export const colors = {
+    branco: '#ffff',
+    cor1: '#ed6464',
+    cor2: '#bf6370',
+    cor3: '#87586c',
+    cor4: '#574759',
+    cor5: '#1a1b1c',
+    preto: '#0000'
+}
---
+++ src/screns/Slide01/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native"
+import { MenuStackTypes } from "../../navigation/MenuStack.navigation";
+
+
+export function Slide01({ navigation }: MenuStackTypes){
+    return(
+        <View>
+            <Text>
+                Olaaaaa
+            </Text>
+            <TouchableOpacity onPress={() => navigation.push("Slide2")}>
+                <Text>
+                    Slide 2
+                </Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
